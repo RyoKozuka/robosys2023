9b5a1e45a844a4d75bf915b2ad9bc3ee07ca369f
Ryo Kozuka <s22c1046ft@s.chibakoudai.jp>
2023-10-26 12:16:52 +0900

    add STRANGE INPUT

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2023 Ryo Kozuka
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,9 +9,17 @@ ng () {
 
 res=0
 
-### I/O TEST ###
+### I/O ###
 out=$(seq 5 | ./plus)
-[ "${out}" = 14.0 ] || ng ${LINENO}
+
+### STRANGE INPUT ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ]		|| ng ${LINENO}
+[ "${out}" = "" ]	|| ng ${LINENO}
+
+out=$(echo | ./plus) #空文字
+[ "$?" = 1 ]		|| ng ${LINENO}
+[ "${out}" = "" ] 	|| ng ${LINENO}
 
 [ "$res" = 0 ] && echo OK
 exit $res
